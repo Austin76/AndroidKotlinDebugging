22f58da790fd571d492f83113031ec8ac2441289
Austin <a_wood1@outlook.com>
2023-09-25 19:13:41 -0400

    Finished project

--- app/src/main/java/com/driuft/androidkotlindebugging/ui/CurrentDayActivity.kt
+++ app/src/main/java/com/driuft/androidkotlindebugging/ui/CurrentDayActivity.kt
@@ -16,7 +16,7 @@ class CurrentDayActivity : AppCompatActivity() {
         setContentView(R.layout.activity_current_day)
 
         currentDayText = findViewById(R.id.current_day_text)
-        currentDayText.text = getString(getDayOfMonth())
+        currentDayText.text = cal.get(Calendar.DAY_OF_MONTH).toString()
     }
 
     private fun getDayOfMonth(): Int {
